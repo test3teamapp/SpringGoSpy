d8ef16d2af387d3715e3dff545e81fb5e99b374c
test3teamapp <test3teamapp@gmail.com>
2023-02-23 15:23:48 +0200

    putting markers on the map

--- src/main/resources/static/js/gmaps/gmapsOperations.js
+++ src/main/resources/static/js/gmaps/gmapsOperations.js
@@ -1,7 +1,30 @@
+var pageGMap;
 function initMap() {
-    // The location of the map
-    var shop = { lat: 50.5039, lng: 4.4699 };
-    var map = new google.maps.Map(
-      document.getElementById('map-container'), { zoom: 4, center: shop });
-    var marker = new google.maps.Marker({ position: shop, map: map });
+   var centerOfInitialMap = { lat: 38.2, lng: 21.7 };
+    pageGMap = new google.maps.Map(
+      document.getElementById('map-container'), { zoom: 7, center: centerOfInitialMap });
+    //addMarker(centerOfInitialMap.lat,centerOfInitialMap.lng);
   }
+
+function addMarker(latitude,longitude){
+    console.log("latitude = " + latitude + " / longitude = " + longitude);
+    var latNum = Number(latitude);
+    var lngNum = Number(longitude);
+     // The location of the map
+     var loc = { lat: latNum, lng: lngNum };
+     var marker = new google.maps.Marker({ position: loc, map: pageGMap });
+     pageGMap.setCenter(loc);
+     pageGMap.setZoom(13);
+
+  }
+
+  function addMarkerWithTitle(latitude,longitude,markerTitle){
+      console.log("latitude = " + latitude + " / longitude = " + longitude);
+      var latNum = Number(latitude);
+      var lngNum = Number(longitude);
+       // The location of the map
+       var loc = { lat: latNum, lng: lngNum };
+       var marker = new google.maps.Marker({ position: loc, map: pageGMap, title: markerTitle });
+       pageGMap.setCenter(loc);
+       pageGMap.setZoom(13);
+    }
