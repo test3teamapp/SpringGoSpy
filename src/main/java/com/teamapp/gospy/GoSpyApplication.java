850a2eb61be4da51203d6933fa5a106338e98499
test3teamapp <test3teamapp@gmail.com>
2023-05-20 23:11:55 +0300

    https  (update calls to api and chat server for use behind traefik reverse proxy)

--- src/main/java/com/teamapp/gospy/GoSpyApplication.java
+++ src/main/java/com/teamapp/gospy/GoSpyApplication.java
@@ -2,6 +2,7 @@ package com.teamapp.gospy;
 
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import com.teamapp.gospy.listeners.AppEventListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRedisDocumentRepositories
 public class GoSpyApplication {
+
+	@Value("${gospy.app.name}")
+	private final String appName = "Spring Boot GoSpy";
 
 	public static void main(String[] args) {
 		SpringApplication springApplication =
--- src/main/java/com/teamapp/gospy/services/PersonDBService.java
+++ src/main/java/com/teamapp/gospy/services/PersonDBService.java
@@ -2,27 +2,57 @@ package com.teamapp.gospy.services;
 
 import com.teamapp.gospy.helperobjects.CommandReplyObj;
 import com.teamapp.gospy.models.Person;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import com.redis.om.spring.search.stream.EntityStream;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
-import java.net.http.HttpClient;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class PersonDBService {
 
     private final String apiHost = "https://gospy.rheotome.eu:8084";
+    private static Logger logger = LoggerFactory.getLogger(PersonDBService.class);
+
+
+    private String trustStorePath;
+    private String trustStorePass;
+    private String keyStorePath;
+    private String keyStorePass;
+    private String keyStoreType;
+    private String keyAlias;
 
     @Autowired
     EntityStream entityStream; // not working for us because of indexing mismatch in redis
@@ -30,12 +60,97 @@ public class PersonDBService {
     // and even we have e.g. @Keyspace(Person) annotation on the Person class
     // the record index is PersonIdx, while the nodejs default was Person:index
 
+    @Autowired
+    private Environment env;
+
     WebClient client;
 
     public PersonDBService() {
-       this.client = WebClient.builder()
-               .baseUrl(apiHost)
-               .build();
+        /*
+        server.ssl.key-store=classpath:gospy_spring_keystore.pkcs12
+server.ssl.key-store-password=rheotome_eu
+server.ssl.key-store-type=PKCS12
+server.ssl.key-alias=rheotome_eu
+server.ssl.key-password=...
+server.ssl.port=8443
+server.ssl.enabled=true
+
+#trust store location
+trust.store=classpath:gospy_spring_truststore
+#trust store password
+trust.store.password=rheotome_eu
+         */
+        /// Environment is null at this point .... :(
+        /// so ...
+
+        this.trustStorePath = "classpath:gospy_spring_truststore";
+        this.trustStorePass = "rheotome_eu";
+        this.keyStorePath = "classpath:gospy_spring_keystore.pkcs12";
+        this.keyStorePass = "rheotome_eu";
+        this.keyStoreType = "PKCS12";
+        this.keyAlias = "rheotome_eu";
+
+        this.client = buildSSLWebClient();
+    }
+
+    private WebClient buildSSLWebClient() {
+        SslContext sslContext;
+        final PrivateKey privateKey;
+        final X509Certificate[] certificates;
+
+        try {
+            final KeyStore trustStore;
+            final KeyStore keyStore;
+            trustStore = KeyStore.getInstance(keyStoreType);
+            trustStore.load(new FileInputStream(ResourceUtils.getFile(trustStorePath)), trustStorePass.toCharArray());
+            keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(new FileInputStream(ResourceUtils.getFile(keyStorePath)), keyStorePass.toCharArray());
+            List<Certificate> certificateList = Collections.list(trustStore.aliases())
+                    .stream()
+                    .filter(t -> {
+                        try {
+                            return trustStore.isCertificateEntry(t);
+                        } catch (KeyStoreException e1) {
+                            throw new RuntimeException("Error reading truststore", e1);
+                        }
+                    })
+                    .map(t -> {
+                        try {
+                            return trustStore.getCertificate(t);
+                        } catch (KeyStoreException e2) {
+                            throw new RuntimeException("Error reading truststore", e2);
+                        }
+                    })
+                    .collect(Collectors.toList());
+            logger.info("Found " + certificateList.size() + " certificates in trust store");
+            certificates = certificateList.toArray(new X509Certificate[certificateList.size()]);
+            privateKey = (PrivateKey) keyStore.getKey(keyAlias, keyStorePass.toCharArray());
+            Certificate[] certChain = keyStore.getCertificateChain(keyAlias);
+            logger.info("Found " + certChain.length + " certificates in key store");
+            X509Certificate[] x509CertificateChain = Arrays.stream(certChain)
+                    .map(certificate -> (X509Certificate) certificate)
+                    .collect(Collectors.toList())
+                    .toArray(new X509Certificate[certChain.length]);
+            sslContext = SslContextBuilder.forClient()
+                    .keyManager(privateKey, keyStorePass, x509CertificateChain)
+                    .trustManager(certificates)
+                    .build();
+
+            reactor.netty.http.client.HttpClient httpClient = HttpClient.create()
+                    .secure(sslContextSpec -> sslContextSpec.sslContext(sslContext));
+            ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
+            return WebClient.builder()
+                    .baseUrl(apiHost)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .clientConnector(connector)
+                    .build();
+
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException |
+                 UnrecoverableKeyException e) {
+            logger.info("Exception while preparing webclient for ssl connection: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     // Find all people
@@ -73,19 +188,19 @@ public class PersonDBService {
     }
 
 
-    public Mono<CommandReplyObj> sendCommandToDevice(String devicename, String command){
+    public Mono<CommandReplyObj> sendCommandToDevice(String devicename, String command) {
         ResponseSpec responseSpec = client.get()
-                .uri(apiHost + "/sendCommand/spring/byName/"+ devicename + "/command/" + command)
+                .uri(apiHost + "/sendCommand/spring/byName/" + devicename + "/command/" + command)
                 .retrieve();
 
-        Mono<CommandReplyObj> responseMonoCommandReplyObj= responseSpec.bodyToMono(CommandReplyObj.class);
+        Mono<CommandReplyObj> responseMonoCommandReplyObj = responseSpec.bodyToMono(CommandReplyObj.class);
 
 
         return responseMonoCommandReplyObj;
     }
 
 
-    public Mono<CommandReplyObj> waitForLUForDevice(String devicename){
+    public Mono<CommandReplyObj> waitForLUForDevice(String devicename) {
         ResponseSpec responseSpec = client.get()
                 .uri(apiHost + "/waitForLU/spring/byName/" + devicename)
                 .retrieve();
